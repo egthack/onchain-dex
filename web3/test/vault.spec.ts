89ad52827c07ce4792583d82d4cc05f6dd1722d0
KeisukeFunatsu <keisukeyan2012@gmail.com>
2025-02-09 03:49:40 +0900

    add basic test

--- web3/test/vault.spec.ts
+++ web3/test/vault.spec.ts
@@ -1,107 +1,179 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
-import hre from "hardhat";
+import { ethers } from "hardhat";
+import { MockERC20 } from "../typechain-types";
+import { Vault } from "../typechain-types";
+import { Signer } from "ethers";
 
-describe("Vault", function () {
-  // // We define a fixture to reuse the same setup in every test.
-  // // We use loadFixture to run this setup once, snapshot that state,
-  // // and reset Hardhat Network to that snapshot in every test.
-  // async function deployOneYearLockFixture() {
-  //   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  //   const ONE_GWEI = 1_000_000_000;
+describe("Vault", async function () {
+  let owner: Signer;
+  let addr1: Signer;
+  let addr2: Signer;
+  let tokenContract: MockERC20;
+  let vaultContract: Vault;
 
-  //   const lockedAmount = ONE_GWEI;
-  //   const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+  const deployFixture = async () => {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+    const TokenFactory = await ethers.getContractFactory("MockERC20");
+    const tokenContract = await TokenFactory.connect(owner).deploy(
+      "Mock Token",
+      "MTK",
+      1000000
+    );
+    await tokenContract.waitForDeployment();
+    await tokenContract.connect(owner).transfer(addr1.address, 1000);
 
-  //   // Contracts are deployed using the first signer/account by default
-  //   const [owner, otherAccount] = await hre.ethers.getSigners();
+    const VaultFactory = await ethers.getContractFactory("Vault");
+    const vaultContract = await VaultFactory.connect(owner).deploy();
+    await vaultContract.waitForDeployment();
 
-  //   const Lock = await hre.ethers.getContractFactory("Lock");
-  //   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    return { owner, addr1, addr2, tokenContract, vaultContract };
+  };
 
-  //   return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  // }
+  describe("Deposit", function () {
+    it("Should deposit tokens and update balance", async function () {
+      ({ tokenContract, vaultContract, addr1 } = await deployFixture());
+      await tokenContract
+        .connect(addr1)
+        .approve(vaultContract.getAddress(), 100);
+      await vaultContract
+        .connect(addr1)
+        .deposit(tokenContract.getAddress(), 100);
 
-  describe("Deployment", function () {
-    //   it("Should set the right unlockTime", async function () {
-    //     const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-    //     expect(await lock.unlockTime()).to.equal(unlockTime);
-    //   });
-    //   it("Should set the right owner", async function () {
-    //     const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-    //     expect(await lock.owner()).to.equal(owner.address);
-    //   });
-    //   it("Should receive and store the funds to lock", async function () {
-    //     const { lock, lockedAmount } = await loadFixture(
-    //       deployOneYearLockFixture
-    //     );
-    //     expect(await hre.ethers.provider.getBalance(lock.target)).to.equal(
-    //       lockedAmount
-    //     );
-    //   });
-    //   it("Should fail if the unlockTime is not in the future", async function () {
-    //     // We don't use the fixture here because we want a different deployment
-    //     const latestTime = await time.latest();
-    //     const Lock = await hre.ethers.getContractFactory("Lock");
-    //     await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-    //       "Unlock time should be in the future"
-    //     );
-    //   });
-    // });
-    // describe("Withdrawals", function () {
-    //   describe("Validations", function () {
-    //     it("Should revert with the right error if called too soon", async function () {
-    //       const { lock } = await loadFixture(deployOneYearLockFixture);
-    //       await expect(lock.withdraw()).to.be.revertedWith(
-    //         "You can't withdraw yet"
-    //       );
-    //     });
-    //     it("Should revert with the right error if called from another account", async function () {
-    //       const { lock, unlockTime, otherAccount } = await loadFixture(
-    //         deployOneYearLockFixture
-    //       );
-    //       // We can increase the time in Hardhat Network
-    //       await time.increaseTo(unlockTime);
-    //       // We use lock.connect() to send a transaction from another account
-    //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-    //         "You aren't the owner"
-    //       );
-    //     });
-    //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-    //       const { lock, unlockTime } = await loadFixture(
-    //         deployOneYearLockFixture
-    //       );
-    //       // Transactions are sent using the first signer by default
-    //       await time.increaseTo(unlockTime);
-    //       await expect(lock.withdraw()).not.to.be.reverted;
-    //     });
-    //   });
-    //   describe("Events", function () {
-    //     it("Should emit an event on withdrawals", async function () {
-    //       const { lock, unlockTime, lockedAmount } = await loadFixture(
-    //         deployOneYearLockFixture
-    //       );
-    //       await time.increaseTo(unlockTime);
-    //       await expect(lock.withdraw())
-    //         .to.emit(lock, "Withdrawal")
-    //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-    //     });
-    //   });
-    //   describe("Transfers", function () {
-    //     it("Should transfer the funds to the owner", async function () {
-    //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-    //         deployOneYearLockFixture
-    //       );
-    //       await time.increaseTo(unlockTime);
-    //       await expect(lock.withdraw()).to.changeEtherBalances(
-    //         [owner, lock],
-    //         [lockedAmount, -lockedAmount]
-    //       );
-    //     });
-    //   });
+      expect(
+        await vaultContract.getBalance(
+          addr1.getAddress(),
+          tokenContract.getAddress()
+        )
+      ).to.equal(100);
+    });
+
+    it("Should revert if deposit amount is 0", async function () {
+      ({ tokenContract, vaultContract, addr1 } = await deployFixture());
+      await expect(
+        vaultContract.connect(addr1).deposit(tokenContract.getAddress(), 0)
+      ).to.be.revertedWith("Deposit: amount must be > 0");
+    });
+  });
+
+  describe("Withdraw", function () {
+    beforeEach(async function () {
+      ({ tokenContract, vaultContract, addr1 } = await deployFixture());
+      await tokenContract
+        .connect(addr1)
+        .approve(vaultContract.getAddress(), 100);
+      await vaultContract
+        .connect(addr1)
+        .deposit(tokenContract.getAddress(), 100);
+    });
+
+    it("Should withdraw tokens and update balance", async function () {
+      await vaultContract
+        .connect(addr1)
+        .withdraw(tokenContract.getAddress(), 50);
+      expect(
+        await vaultContract.getBalance(
+          addr1.getAddress(),
+          tokenContract.getAddress()
+        )
+      ).to.equal(50);
+    });
+
+    it("Should revert if withdraw amount exceeds balance", async function () {
+      await expect(
+        vaultContract.connect(addr1).withdraw(tokenContract.getAddress(), 200)
+      ).to.be.revertedWith("Withdraw: insufficient balance");
+    });
+
+    it("Should revert if withdraw amount is 0", async function () {
+      await expect(
+        vaultContract.connect(addr1).withdraw(tokenContract.getAddress(), 0)
+      ).to.be.revertedWith("Withdraw: amount must be > 0");
+    });
+  });
+
+  describe("Internal Transfer", function () {
+    beforeEach(async function () {
+      ({ tokenContract, vaultContract, owner, addr1, addr2 } =
+        await deployFixture());
+      await tokenContract
+        .connect(addr1)
+        .approve(vaultContract.getAddress(), 100);
+      await vaultContract
+        .connect(addr1)
+        .deposit(tokenContract.getAddress(), 100);
+    });
+
+    it("Should transfer tokens internally and update balances", async function () {
+      await vaultContract
+        .connect(owner)
+        .internalTransfer(
+          addr1.getAddress(),
+          addr2.getAddress(),
+          tokenContract.getAddress(),
+          50
+        );
+      expect(
+        await vaultContract.getBalance(
+          addr1.getAddress(),
+          tokenContract.getAddress()
+        )
+      ).to.equal(50);
+      expect(
+        await vaultContract.getBalance(
+          addr2.getAddress(),
+          tokenContract.getAddress()
+        )
+      ).to.equal(50);
+    });
+
+    it("Should revert if transfer amount exceeds balance", async function () {
+      const { tokenContract, vaultContract, owner, addr1, addr2 } =
+        await deployFixture();
+      await expect(
+        vaultContract
+          .connect(owner)
+          .internalTransfer(
+            addr1.getAddress(),
+            addr2.getAddress(),
+            tokenContract.getAddress(),
+            200
+          )
+      ).to.be.revertedWith("InternalTransfer: insufficient balance");
+    });
+
+    it("Should revert if transfer amount is 0", async function () {
+      const { tokenContract, vaultContract, owner, addr1, addr2 } =
+        await deployFixture();
+      await expect(
+        vaultContract
+          .connect(owner)
+          .internalTransfer(
+            addr1.address,
+            addr2.address,
+            tokenContract.getAddress(),
+            0
+          )
+      ).to.be.revertedWith("InternalTransfer: amount must be > 0");
+    });
+  });
+
+  describe("Get Balance", function () {
+    beforeEach(async function () {
+      ({ tokenContract, vaultContract, addr1 } = await deployFixture());
+      await tokenContract
+        .connect(addr1)
+        .approve(vaultContract.getAddress(), 100);
+      await vaultContract
+        .connect(addr1)
+        .deposit(tokenContract.getAddress(), 100);
+    });
+
+    it("Should return the correct balance", async function () {
+      const balance = await vaultContract.getBalance(
+        addr1.getAddress(),
+        tokenContract.getAddress()
+      );
+      expect(balance).to.equal(100);
+    });
   });
 });
